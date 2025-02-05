cf5a4f8d2a076c8bc4ff5f2a2cc69c1c95fa8fd6
Guilherme Alves Dos Santos <104768721+GUIZO2307@users.noreply.github.com>
2025-02-04 23:09:54 -0300

    inserção de projetos

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,17 @@
-const botaoMostraProjetos = document.querySelector('.btn-mostra-projetos');
+const botaoMostraProjetos = document.querySelector(".btn-mostra-projetos");
 
-botaoMostraProjetos.addEventListener('click', () => {
-    mostrarMaisProjetos();
-    esconderBotao();
-})
+botaoMostraProjetos.addEventListener("click", () => {
+  mostrarMaisProjetos();
+  esconderBotao();
+});
 
 function mostrarMaisProjetos() {
-    const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)');
-    projetosInativos.forEach(projeto => {
-        projeto.classList.add('ativo');
-    });
+  const projetosInativos = document.querySelectorAll(".projeto:not(.ativo)");
+  projetosInativos.forEach((projeto) => {
+    projeto.classList.add("ativo");
+  });
 }
 
 function esconderBotao() {
-    botaoMostraProjetos.classList.add("remover");
+  botaoMostraProjetos.classList.add("remover");
 }
